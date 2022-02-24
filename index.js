e5f903b0fa476e0cfcd9e19e3abb99693afe33d1
Naima Hassan <naima.hassan434@gmail.com>
2022-02-24 19:40:10 +0000

    POST a new product

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const { v4: uuidv4 } = require("uuid");
+
 //Basic route
 app.get("/", (req, res) => {
   res.send("Hello, world!");
@@ -28,7 +30,17 @@ app.get("/api/products/:id", (req, res) => {
   return res.json(product);
 });
 //Insert a new product
+app.use(express.json());
+app.post("/api/products", (req, res) => {
+  const product = {
+    id: uuidv4(),
+    name: req.body.name,
+    price: req.body.price,
+  };
 
+  products.push(product);
+  return res.json(product);
+});
 //Update a specific product(PUT)
 
 //Update a specific product(PATCH)
