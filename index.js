203fa1ff01da5959245f5e32f5dbb1fe4b91f8ec
Naima Hassan <naima.hassan434@gmail.com>
2022-02-24 19:13:46 +0000

    Get all products

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+const products = [
+  { id: 1, name: "Mango", price: 234 },
+  { id: 2, name: "Orange", price: 154 },
+  { id: 3, name: "Blackberry", price: 214 },
+  { id: 4, name: "Melon", price: 134 },
+  { id: 5, name: "Apple", price: 200 },
+];
+//Show list of products
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+//show a specific product
+
+//Insert a new product
+
+//Update a specific product(PUT)
+
+//Update a specific product(PATCH)
+
+//Delete a specific product
+//Delete all products
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
